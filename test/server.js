19960d67368d601655a6dc3270f868d47fee5a4c
Ubuntu <noma@dasasaki-vm-discord-sktrythmjs.ncckohtjsfguhjju31zcbrmmgb.lx.internal.cloudapp.net>
2021-10-04 17:28:15 +0000

    test code

--- test/server.js
+++ test/server.js
@@ -4,8 +4,5 @@ var getYouTubeTitle = require('../')
 test('server', function (t) {
   t.plan(2)
 
-  getYouTubeTitle('QS_59uXCqHM', function (err, result) {
-    t.ifError(err)
-    t.equal(result, '소유(Soyou) X 권정열_ 어깨 (Lean on me) Music Video (Soyou x Kwon Jeongyeol)')
-  })
+  getYouTubeTitle('QS_59uXCqHM')
 })
